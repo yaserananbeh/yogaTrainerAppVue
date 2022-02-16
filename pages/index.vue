e0d20e7bd96a770c8cea9684fbcdbdaf7d3196fc
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-02-16 17:00:08 +0200

    add the carousal

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  
+  <Carousel />
 </template>
 
 <script>
+import Carousel from "../components/Carousel.vue";
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  components: {
+    Carousel,
+  },
+};
 </script>
